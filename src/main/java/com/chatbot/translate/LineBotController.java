c37e0bdb059aaa411fc34da1e0ecd9c7eb349327
marcell17002 <marcellantonius18@gmail.com>
2018-12-19 15:13:58 +0700

    balik lagi

--- src/main/java/com/chatbot/translate/LineBotController.java
+++ src/main/java/com/chatbot/translate/LineBotController.java
@@ -84,10 +84,10 @@ public class LineBotController
                 if (!msgText.contains("bot leave")){
                     if(msgText.contains("Siapa yang membuat mu ? ")){
 
-                        String balas = "created : Marcell Antonius";
+                        String balas = "created : Marcell Antonius dan Felia Sri Indriyani";
                         replyToUser(payload.events[0].replyToken, balas);
                     }
-                    if(msgText.contains("Hi")){
+                    if(msgText.contains("hi")){
 
                         String balas = "Hi , Kenalin aku T-Teen aku akan membantumu untuk merekomendasikan menu makananan hari ini yang sesuai dengan mood mu hari ini . Boleh aku tahu bagaimanakah perasaanmu hari ini ? ";
                         replyToUser(payload.events[0].replyToken, balas);
@@ -97,21 +97,27 @@ public class LineBotController
                         String balas = "Jika kamu sedang sedih, aku merekomendasikanmu makan makanan yang kaya akan Omega-3. Tujuannya adalah untuk meningkatkan moodmu hari ini  serta mengatasi tekanan agar tidak semakin larut dalam kesedihan . Makanan yang kaya Omega-3 antara lain: ikan-ikanan, telur, daging merah . Nah , dari pilihan itu kamu pilih yang mana ? ";
                         replyToUser(payload.events[0].replyToken, balas);
                     }
-                    if(msgText.contains("Ikan")){
-
-                        String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . ikan bakar , ikan goreng , ikan sayur . Kamu mau pilih yang mana ? ";
-                        replyToUser(payload.events[0].replyToken, balas);
-                    }
-                    if(msgText.contains("Ikan Bakar")){
-
-                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Bale , Kantin FK dan Kantin Psikologi disana meneyediakan makanan yang kamu pilih . :)) ";
-                        replyToUser(payload.events[0].replyToken, balas);
-                    }
                     if(msgText.contains("Marah")){
 
                         String balas = "Saat sedang marah jangan biarkan emosi Anda meledak-ledak dan menguasai. Hasilnya akan berbahaya lho. Mengatasi hal ini lebih baik Anda menenangkan diri dengan mengonsumsi makanan yang kaya akan zinc. Menurut penelitian di Jepang, seperti dikutip dari ivillage.co.uk, zinc dapat membantu mengendalikan emosi dan meredakan kemarahan. Makanan yang kaya zinc antara lain: daging merah, ayam, produk susu, makanan laut, gandum-ganduman, kacang-kacangan. Nah , dari pilihan itu kamu pilih yang mana ?"    ;
                         replyToUser(payload.events[0].replyToken, balas);
                     }
+                    if(msgText.contains("Jatuh cinta")){
+
+                        String balas = "Sedang jatuh cinta dan berbunga-bunga? Ada beberapa makanan yang akan membuatmu semakin ceria dan menambah gairah asmara cinta. Makanan yang harus diasup adalah yang mengandung zinc, arginine, selenium dan niacin. Makanan ini bisa disebut juga sebagai afrodisiak. Membangkitkan gairah dalam hal cinta serta seksual. Di antara lain seperti: kuaci labu, tiram, cabai, cokelat, daging kambing, alpukat, ikan-ikanan, dan lain sebagainya. Nah , dari pilihan itu kamu pilih yang mana ?";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ga pede")){
+
+                        String balas = "Kalau sedang tak percaya diri ada beberapa makanan yang bisa disantap untuk meningkatkan kepercayaan diri Anda.Para peneliti di McGill University Monteral mengatakan bahwa mengatasi kurang percaya diri bisa dengan mengonsumsi makanan yang mengandung tryptophan. Makanan yang menjadi sumber tryptophan antara lain: daging (terutama ayam), ikan (terutama salmon dan tuna). Nah , dari pilihan itu kamu pilih yang mana ?";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ikan")) {
+
+                        String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . ikan bakar , ikan goreng , ikan sayur . Kamu mau pilih yang mana ? ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+
                     if(msgText.contains("Daging merah")){
 
                         String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . Tenderloin , sop buntut  . Kamu mau pilih yang mana ? ";
@@ -122,29 +128,99 @@ public class LineBotController
                         String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FIB dan Kantin Psikologi disana meneyediakan makanan yang kamu pilih . :))";
                         replyToUser(payload.events[0].replyToken, balas);
                     }
-                    if(msgText.contains("Jatuh cinta")){
-
-                        String balas = "Sedang jatuh cinta dan berbunga-bunga? Ada beberapa makanan yang akan membuatmu semakin ceria dan menambah gairah asmara cinta. Makanan yang harus diasup adalah yang mengandung zinc, arginine, selenium dan niacin. Makanan ini bisa disebut juga sebagai afrodisiak. Membangkitkan gairah dalam hal cinta serta seksual. Di antara lain seperti: kuaci labu, tiram, cabai, cokelat, daging kambing, alpukat, ikan-ikanan, dan lain sebagainya. Nah , dari pilihan itu kamu pilih yang mana ?";
-                        replyToUser(payload.events[0].replyToken, balas);
-                    }
                     if(msgText.contains("Daging kambing")){
 
                         String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . sop iga kambing , iga kambing bakar . Kamu mau pilih yang mana ? ";
                         replyToUser(payload.events[0].replyToken, balas);
                     }
-                    if(msgText.contains("Sop iga kambing")){
-
-                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FIB dan Kantin Psikologi disana meneyediakan makanan yang kamu pilih . :) ";
-                        replyToUser(payload.events[0].replyToken, balas);
-                    }
-                    if(msgText.contains("Ga pede")){
-
-                        String balas = "Kalau sedang tak percaya diri ada beberapa makanan yang bisa disantap untuk meningkatkan kepercayaan diri Anda.Para peneliti di McGill University Monteral mengatakan bahwa mengatasi kurang percaya diri bisa dengan mengonsumsi makanan yang mengandung tryptophan. Makanan yang menjadi sumber tryptophan antara lain: daging (terutama ayam), ikan (terutama salmon dan tuna). Nah , dari pilihan itu kamu pilih yang mana ?";
-                        replyToUser(payload.events[0].replyToken, balas);
-                    }
                     if(msgText.contains("Ayam")){
 
                         String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . ayam goreng , ayam geprek, ayam rica-rica, ayam sayur, dan ayam karedok . Kamu mau pilih yang mana ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Pecel sayur")) {
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca disana meneyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Karedok")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin PPBS disana meneyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Telur balado")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FK dan Kantin Statistika disana meneyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ikan sayur")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FK dan Cantina MIPA disana menyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ikan goreng")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca, Kantin FK dan Kantin Statistika disana menyediakan makanan yang kamu pilih . :)) ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ikan bakar")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Bale , Kantin FK dan Kantin Psikologi disana menyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Tenderloin")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin FK disana menyediakan makanan yang kamu pilih . :) ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Sop iga kambing")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FIB dan Kantin Psikologi disana menyediakan makanan yang kamu pilih . :)";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Sop buntut")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FIB dan Kantin Psikologi disana menyediakan makanan yang kamu pilih . :))";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ayam karedok")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin FK , Kantin FIB, Kantin PPBS dan Kantin Psikologi disana menyediakan makanan yang kamu pilih . :)";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ayam sayur")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin FIB, dan Kantin Statistika disana menyediakan makanan yang kamu pilih . :)";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ayam rica-rica")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin Statistika, dan Kantin Pertanian disana menyediakan makanan yang kamu pilih . :)";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ayam geprek")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin PPBS dan Kantin Bale Wilasa 7 disana menyediakan makanan yang kamu pilih . :) ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Kacang")){
+
+                        String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . karedok , pecel sayur . Kamu mau pilih yang mana ? ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Telur")){
+
+                        String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . telur dadar , telur balado  . Kamu mau pilih yang mana ? ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Ayam geprek")){
+
+                        String balas = "kamu bisa pergi ke tempat makan berikut . Kantin Pedca , Kantin PPBS dan Kantin Bale Wilasa 7 disana menyediakan makanan yang kamu pilih . :) ";
+                        replyToUser(payload.events[0].replyToken, balas);
+                    }
+                    if(msgText.contains("Kacang")){
+
+                        String balas = "Nah berikut ini adalah menu makananan hasil rekomendasiku untuk makanan yang kamu pilih . karedok , pecel sayur . Kamu mau pilih yang mana ? ";
                         replyToUser(payload.events[0].replyToken, balas);
                     }
 
