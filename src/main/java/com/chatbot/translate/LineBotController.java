0345f3c60040bc8b8eb10b253e4b8f3455b0f1aa
marcell17002 <marcellantonius18@gmail.com>
2018-12-19 15:20:44 +0700

    balik

--- src/main/java/com/chatbot/translate/LineBotController.java
+++ src/main/java/com/chatbot/translate/LineBotController.java
@@ -148,6 +148,20 @@ public class LineBotController
                         replyToUser(payload.events[0].replyToken, balas);
                     }
 
+//                    String fromLang = "id";
+//                    String toLang = "su";
+//                    //String tex = "Let's have some fun!";
+//
+//                    translate(fromLang, toLang, msgText, payload.events[0].replyToken);
+//
+//                    //replyToUser(payload.events[0].replyToken, msgText);
+//                    /*try {
+//                        getMessageData(msgText, idTarget);
+//                    } catch (IOException e) {
+//                        System.out.println("Exception is raised ");
+//                        e.printStackTrace();
+//                    }
+//                    */
                     else{
                         String balas = "Mohon maaf fitur ini akan terus diperbaiki untuk menunjang kenyamanan anda sekalian :))" ;
                         replyToUser(payload.events[0].replyToken, balas);
@@ -168,47 +182,7 @@ public class LineBotController
         return new ResponseEntity<String>(HttpStatus.OK);
     }
 
-    private void translate(String fromLang, String toLang, String text, String payload) throws IOException {
-        // TODO: Should have used a 3rd party library to make a JSON string from an object
-        String jsonPayload = new StringBuilder()
-                .append("{")
-                .append("\"fromLang\":\"")
-                .append(fromLang)
-                .append("\",")
-                .append("\"toLang\":\"")
-                .append(toLang)
-                .append("\",")
-                .append("\"text\":\"")
-                .append(text)
-                .append("\"")
-                .append("}")
-                .toString();
 
-        URL url = new URL(ENDPOINT);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        conn.setDoOutput(true);
-        conn.setRequestMethod("POST");
-        conn.setRequestProperty("X-WM-CLIENT-ID", CLIENT_ID);
-        conn.setRequestProperty("X-WM-CLIENT-SECRET", CLIENT_SECRET);
-        conn.setRequestProperty("Content-Type", "application/json");
-
-        OutputStream os = conn.getOutputStream();
-        os.write(jsonPayload.getBytes());
-        os.flush();
-        os.close();
-
-        int statusCode = conn.getResponseCode();
-        System.out.println("Status Code: " + statusCode);
-        BufferedReader br = new BufferedReader(new InputStreamReader(
-                (statusCode == 200) ? conn.getInputStream() : conn.getErrorStream()
-        ));
-        String output;
-        while ((output = br.readLine()) != null) {
-            replyToUser(payload, output);
-            //System.out.println(output);
-        }
-        conn.disconnect();
-    }
 
     private void getMessageData(String message, String targetID) throws IOException{
         if (message!=null){
@@ -271,5 +245,3 @@ public class LineBotController
         }
     }
 }
-
-
